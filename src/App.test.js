4ee6dcb219c7cb29154cb0446ac76154e04cda8a
Michael <michael.egwali@andela.com>
2021-03-18 10:26:27 +0100

    Set up App skeleton

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { create } from 'react-test-renderer';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('App', () => {
+  it('renders', () => {
+    const component = create(<App />);
+    expect(component.toJSON()).toMatchSnapshot();
+  })
 });
