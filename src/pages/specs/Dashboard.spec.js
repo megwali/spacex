e8a60ff5a0fe8ada9add4ad4ccfdfca59013a45b
Michael <michael.egwali@andela.com>
2021-03-19 00:33:19 +0100

    update mocks and snapshot test

--- src/pages/specs/Dashboard.spec.js
+++ src/pages/specs/Dashboard.spec.js
@@ -4,6 +4,7 @@ import Dashboard from '../Dashboard';
 
 jest.mock('react-router-dom', () => ({ Link: 'Link' }));
 jest.mock('../styledComponents', () => ({ CardList: 'CardList', Error: 'Error', Loader: 'Loader' }));
+jest.mock('../../components/LaunchCard', () => 'LaunchCard');
 jest.mock('../../hooks/useLaunchList', () => () => ({
   launches: [{ flight_number: 1, launch_date_local: '2020-08-30T19:18:00-04:00' }],
   loading: false,
